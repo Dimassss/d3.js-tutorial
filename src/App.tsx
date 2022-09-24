e2aad638ec4c2eb010fe735b02e0e2ad77f6fd93
Dimassss <dimas.karpusss@gmail.com>
2022-09-25 00:47:36 +0200

    Prepare for d3.js doc tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,7 @@ import {
     Route,
 } from "react-router-dom";
 import ErrorPage from './error-page';
-import HomePage from './pages';
-import CssColourNames from './pages/css-colour-names';
-import ReactMouseCircleFollower from './pages/react-mouse-circle-follower';
-import SmilePage from './pages/smile';
-import VegaPage from './pages/vega';
+import HomePage from './pages/home';
 
 
 const router = createBrowserRouter([
@@ -17,22 +13,6 @@ const router = createBrowserRouter([
         path: "/",
         element: (<HomePage/>),
         errorElement: <ErrorPage />,
-    },
-    {
-        path: 'smile',
-        element: (<SmilePage/>)
-    },
-    {
-        path: 'react-mouse-circle-follower',
-        element: (<ReactMouseCircleFollower/>)
-    },
-    {
-        path: 'css-colour-names',
-        element: (<CssColourNames/>)
-    },
-    {
-        path: 'vega',
-        element: (<VegaPage/>)
     }
 ]);
 
